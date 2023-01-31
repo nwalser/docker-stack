2b4c1736a0b13e99b8601548e4f29cb16cdd0298
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-31 22:11:48 +0100

    added tailwind

---
+++ src/src/components/Prose.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import clsx from 'clsx'
+</script>
+
+<div class={clsx('prose prose-invert')}>
+    <slot />
+</div>
---
+++ src/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from 'src/components/Header.svelte';
+import Prose from 'src/components/Prose.svelte';
+	import '../app.css';
+</script>
+
+
+<Header />
+
+<Prose>
+	<slot />
+</Prose>
